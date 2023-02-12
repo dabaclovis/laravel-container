230fbcbdd00eb268e3f1243f72d91f14e5fe2924
daba clovis <dabaclovis@yahoo.com>
2023-02-12 01:20:05 -0500

    'tables'

--- database/migrations/2023_02_12_061151_create_articles_table.php
+++ database/migrations/2023_02_12_061151_create_articles_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->longText('body');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
